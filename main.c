1f14673a89b40912cf27fa31dd706d53874ace0e
Alibion18 <alifatihars@gmail.com>
2024-10-20 19:28:37 +0300

    18.10.2024 karekök bulma ödevi

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {	
+	double x,x_sqrt;
+
+    printf("Karekok Bulmak icin sayiyi giriniz:\n");
+    scanf("%lf",&x);
+    x_sqrt=sqrt(x);
+    printf("%f\n",x_sqrt);
+	
+	
+	return 0;
+}
